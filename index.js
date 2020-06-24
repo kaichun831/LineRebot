7b33d4d8aacd575382e0e3a05057f33808e3f97c
kaichun831 <kaichun831@gmail.com>
2020-06-24 14:35:34 +0800

    hero

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-var express = require('express');
-
-var bot = require('./src');
-
-const app = express();
-app.listen(80, () => {
-  console.log('App listening on port 3000!');
-});
-app.get('/', (req, res) => {
-  res.send('run on 6666');
-});
-app.use('/', bot);
-module.exports = app;
-
 module.exports = require('./src');
