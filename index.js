b56c0a9648c22f7af7a61cc838ab92c9367512e0
kaichun831 <kaichun831@gmail.com>
2020-06-24 14:22:54 +0800

    run

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bot = require('./src');
 
 const app = express();
-app.listen(3000, () => {
+app.listen(80, () => {
   console.log('App listening on port 3000!');
 });
 app.get('/', (req, res) => {
@@ -12,4 +12,4 @@ app.get('/', (req, res) => {
 app.use('/', bot);
 module.exports = app;
 
-// module.exports = require('./src');
+module.exports = require('./src');
