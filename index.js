d934baf88c9c3144b121ede3b9cd9e80a336f647
kaichun831 <kaichun831@gmail.com>
2020-06-24 15:39:42 +0800

    x

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-// var express = require('express');
-// var bot = require('./src');
-
-// const app = express();
-// app.listen(80, () => {
-//   console.log('App listening on port 80!');
-// });
-// app.get('/', (req, res) => {
-//   res.send('run on 6666');
-// });
 module.exports = require('./src');
